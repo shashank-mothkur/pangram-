cfe2fc16305f288ad2f882de7160f10320eca7e8
shashank-mothkur <69675014+shashank-mothkur@users.noreply.github.com>
2020-08-15 15:46:32 +0530

    Update pangram.py

--- pangram.py
+++ pangram.py
@@ -1,6 +1,6 @@
 sentence=str(input("enter the string")).lower()
 converting_string_into_set=set(sentence)
-if(len(converting_string_into_set)==27):
+if(len(converting_string_into_set)<=27):
     print("pangram")
 else:
     print("not pangram")
