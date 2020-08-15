81e93f5ff93f2058a46fcf6786c051ad7bb26634
shashank-mothkur <69675014+shashank-mothkur@users.noreply.github.com>
2020-08-15 14:19:26 +0530

    Add files via upload

---
+++ pangram.py
@@ -0,0 +1,6 @@
+sentence=str(input("enter the string")).lower()
+converting_string_into_set=set(sentence)
+if(len(converting_string_into_set)==27):
+    print("pangram")
+else:
+    print("not pangram")
